26c5e639c3a9133535af225bee08a8056b70c7b7
Ilya <imoldavskiy@yansex.ru>
2021-12-07 00:52:17 +0300

    BishopPieceService get possible moves need to check again Start Rook

--- src/main/java/ru/vsu/cs/oop2021/g41/moldavskiy_i_m/oop/service/RookPieceService.java
+++ src/main/java/ru/vsu/cs/oop2021/g41/moldavskiy_i_m/oop/service/RookPieceService.java
@@ -4,17 +4,61 @@ import ru.vsu.cs.oop2021.g41.moldavskiy_i_m.oop.model.Cell;
 import ru.vsu.cs.oop2021.g41.moldavskiy_i_m.oop.model.Game;
 import ru.vsu.cs.oop2021.g41.moldavskiy_i_m.oop.model.Piece;
 import ru.vsu.cs.oop2021.g41.moldavskiy_i_m.oop.model.Step;
+import ru.vsu.cs.oop2021.g41.moldavskiy_i_m.oop.model.enums.DirectionEnum;
 
-import java.util.List;
+import java.util.*;
 
-public class RookPieceService implements IPieceService{
+public class RookPieceService implements IPieceService {
     @Override
     public List<Cell> getPossibleMoves(Game game, Piece piece) {
-        return null;
+        List<Cell> possibleMoves = new ArrayList<>();
+        //Set<Cell> possibleMoves = new LinkedHashSet<>();
+        Set<Cell> beatMoves = new LinkedHashSet<>();
+        List<DirectionEnum> directionEnumList =
+                Arrays.asList(DirectionEnum.NORTH, DirectionEnum.EAST,
+                        DirectionEnum.SOUTH, DirectionEnum.WEST);
+        return possibleMoves;
     }
+
 
     @Override
     public Step makeMove(Game game) {
         return null;
+    }
+
+    private List<Cell> findRookMoves(Game game, Piece piece, List<DirectionEnum> directionEnumList) {
+        List<Cell> possibleMoves = new ArrayList<>();
+        Cell receivedCell = game.getPiece2CellMap().get(piece);
+        Cell currentCell;
+        Cell nextCell;
+        DirectionEnum direction;
+
+        for (int i = 0; i < directionEnumList.size(); i++) {
+            direction = directionEnumList.get(i);
+            nextCell = receivedCell.getNeighbors().get(direction);
+            while (isMoveAvailable(game, piece, nextCell)) {
+                possibleMoves.add(nextCell);
+                currentCell = nextCell;
+                nextCell = currentCell.getNeighbors().get(direction);
+                if(checkEnemyPieceCell(game, piece, nextCell) && checkEnemyPieceCell(game, piece, currentCell)) {
+                    break;
+                }
+            }
+        } //TODO перепроверить условие в Слоне и Ладье, ещё раз проверить Слона. Попробовать комбинацию checkEnemyPieceCell(game, piece, nextCell) И isMoveAvailable(game, piece, nextCell)
+        return possibleMoves;
+    }
+
+    private boolean isMoveAvailable(Game game, Piece piece, Cell testedCell) {
+        if (testedCell != null) {
+            return ((game.getCell2PieceMap().get(testedCell) == null) ||
+                    ((game.getCell2PieceMap().get(testedCell) != null) &&
+                            (game.getCell2PieceMap().get(testedCell).getPieceColor() != piece.getPieceColor())));
+        }
+        return false;
+    }
+
+    private boolean checkEnemyPieceCell(Game game,Piece piece, Cell testedCell) {
+        return (game.getCell2PieceMap().get(testedCell) != null) &&
+                (game.getCell2PieceMap().get(testedCell).getPieceColor() != piece.getPieceColor());
     }
 }
